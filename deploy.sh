dd83212c097bb53b2cb52bc118217169a49c3c39
Nemikolh <joan.varvenne@gmail.com>
2015-09-16 11:49:11 +0100

    Fixed an npm install problem and updated dependencies.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 sudo apt-get install -y mongodb-server
 sudo apt-get install -y curl
+sudo apt-get install -y libkrb5-dev
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
