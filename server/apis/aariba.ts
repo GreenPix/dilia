fdf5be95001aeee42e9f8f280e63a3f6b0f57d7a
Nemikolh <joan.varvenne@gmail.com>
2015-10-18 14:11:24 +0100

    Aariba API: (commit/get)Rev and obtain current.

--- server/apis/aariba.ts
+++ server/apis/aariba.ts
@@ -1,5 +1,5 @@
 import {User, UserDocument} from '../db/schemas/users';
-import {AaribaScript, AaribaScriptSchema} from '../db/schemas/aariba';
+import {AaribaScript, AaribaScriptProperties} from '../db/schemas/aariba';
 import {app} from '../config/express';
 import {authenticate} from 'passport';
 import {reqAuth} from './middlewares';
@@ -13,7 +13,7 @@ import _ = require('lodash');
 // Get the list of scripts (name and lock status)
 app.get('/api/aariba/', reqAuth, (req, res) => {
     AaribaScript.find({})
-    .select('name locked_by')
+    .select('name')
     .exec((err, scripts) => {
         res.status(200);
         res.json(_.reduce(scripts, (res, val) => {
@@ -24,27 +24,6 @@ app.get('/api/aariba/', reqAuth, (req, res) => {
             };
             return res;
         }, {}));
-    });
-});
-
-// Lock a particular script
-app.post('/api/aariba/:name/lock', reqAuth, (req, res) => {
-    AaribaScript.findOne({ name: req.params.name }, (err, script) => {
-        if (err || !script) {
-            werror(`Error while trying to lock ${req.params.name}: ${err}`);
-            res.sendStatus(404);
-        } else {
-            script.locked_by = req.user._id;
-            script.save((err) => {
-                if (err) {
-                    werror(`Couldn't lock script: ${req.params.name}` +
-                     `for user ${req.user.username}`);
-                    res.status(500);
-                } else {
-                    res.status(200);
-                }
-            });
-        }
     });
 });
 
@@ -61,18 +40,62 @@ app.get('/api/aariba/:name', reqAuth, (req, res) => {
     });
 });
 
+// Obtain the content of a script at a particular revision
+app.get('/api/aariba/:name/revision/:id', reqAuth, (req, res) => {
+    AaribaScript.findOne({ name: req.params.name }, (err, script) => {
+        if (err || !script) {
+            werror(err);
+            res.sendStatus(404);
+        } else {
+            res.status(200);
+            res.json(script.getRevision(req.params.id));
+        }
+    });
+});
+
+// Commit a new revision of a script
+app.post('/api/aariba/:name/commit', reqAuth, (req, res) => {
+    let user: UserDocument = req.user;
+    AaribaScript.findOne({ name: req.params.name }, (err, script) => {
+        if (err || !script) {
+            werror(err);
+            res.sendStatus(400);
+        } else {
+            script.commitRevision({
+                author: user._id,
+                comment: req.body.comment,
+                content: 'TODO',
+            }, (err) => {
+                if (err) {
+                    werror(`Couldn't save script: '${script.name}'`);
+                    res.status(500).json(errorToJson(err));
+                } else {
+                    winfo(`Committed new version for '${script.name}'`);
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+})
+
+// Create a new script aariba
 app.post('/api/aariba/new', reqAuth, (req, res, next) =>  {
     let user: UserDocument = req.user;
-    let properties = <AaribaScriptSchema>_.merge({
+    let properties: AaribaScriptProperties = {
         contributors: [user._id],
-        locked_by: user._id,
-    }, req.body);
+        revisions: [{
+            author: user._id,
+            content: req.body.content,
+            comment: req.body.comment,
+        }],
+        name: req.body.name
+    };
     let script = new AaribaScript(properties);
     script.save((err) => {
         if (err) {
             // next(err);
             werror(`Couldn't save script: ${properties.name}`);
-            res.status(400).json(errorToJson(err));
+            res.status(500).json(errorToJson(err));
         } else {
             // Saved new script !
             winfo(`Saved new script '${properties.name}'!`);
--- server/db/schemas/aariba.ts
+++ server/db/schemas/aariba.ts
@@ -4,25 +4,62 @@ import _ = require('lodash');
 
 /// Schema:
 let mongooseAaribaScriptSchema = new Schema({
-    name: String,
+    name: { type : String, maxlength: [
+        30,
+        'Name (`{VALUE}`) exceeds the ' +
+        'maximum allowed length ({MAXLENGTH}).'
+    ] },
     created_on: { type: Date, default: Date.now },
-    content: String,
+    revisions: [{
+        author: Schema.Types.ObjectId,
+        comment: { type: String, maxlength: [
+            60,
+            'Comment (`{VALUE}`) exceeds the ' +
+            'maximum allowed length ({MAXLENGTH}).'
+        ] },
+        content: String,
+        date: { type: Date, default: Date.now },
+    }],
     contributors: [Schema.Types.ObjectId],
-    locked_by: Schema.Types.ObjectId,
 });
 
-/// Schema (Typescript equivalent)
-export interface AaribaScriptSchema {
-    name: string;
-    created_on: Date;
+export interface Revision {
+    author: Types.ObjectId;
     content: string;
-    contributors: Types.ObjectId[];
-    locked_by: Types.ObjectId;
-
-    // Form usefull for the client
-    toJsonResponse(): string;
+    comment: string;
+    date?: Date;
 }
 
+/// Schema (Typescript equivalent)
+export interface AaribaScriptProperties {
+    name: string;
+    created_on?: Date;
+    revisions: Array<Revision>;
+    contributors: Types.ObjectId[];
+}
+
+export interface AaribaScriptSchema extends AaribaScriptProperties {
+    // Form usefull for the client
+    toJsonResponse(): any;
+    getRevision(id: number): any;
+    commitRevision(rev: Revision, cb: (err: any) => void): void;
+}
+
+mongooseAaribaScriptSchema.path('revisions').validate(function (revisions: Array<Revision>) {
+    return revisions.length;
+}, 'Revisions cannot be empty');
+
+mongooseAaribaScriptSchema.path('revisions').validate(function (revisions: Array<Revision>, cb) {
+    if (this.isNew || this.isModified('revisions')) {
+        for (let rev of revisions) {
+            if (!rev.comment || !rev.comment.length) {
+                cb(false);
+                return;
+            }
+        }
+    }
+    cb(true);
+}, 'Each revision must contains a non-empty comment');
 
 mongooseAaribaScriptSchema.path('name').validate(function (name) {
     return name.length;
@@ -39,10 +76,40 @@ mongooseAaribaScriptSchema.path('name').validate(function (name, cb) {
 }, 'Name already exists');
 
 mongooseAaribaScriptSchema.method({
-    toJsonResponse: function (): string {
-        return _.pick<any, AaribaScriptSchema>(this,
-            ['name', 'content', 'created_on']
+    toJsonResponse: function (): any {
+        let self: AaribaScriptSchema = this;
+        let res =  _.pick<any, AaribaScriptSchema>(self,
+            ['name', 'created_on']
         );
+
+        res.content = self.revisions[0].content;
+        res.revisions = self.revisions.map(r => {
+            return {
+                author: r.author,
+                date: r.date,
+                comment: r.comment,
+            };
+        });
+
+        return res;
+    },
+
+    getRevision: function (id: number): any {
+        let self: AaribaScriptSchema = this;
+        return self.revisions[id];
+    },
+
+    commitRevision: function (rev: Revision, cb: (err: any) => void): void {
+        let self: AaribaScriptDocument = this;
+        self.revisions.push({
+            author: rev.author,
+            comment: rev.comment,
+            content: rev.content,
+        });
+        if (!_.find(self.contributors, contrib => contrib.equals(rev.author))) {
+            self.contributors.push(rev.author);
+        }
+        self.save(cb);
     }
 });
 
