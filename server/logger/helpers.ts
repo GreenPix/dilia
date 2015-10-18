614614be296c949fbf51c911fcdc58f6d3a32154
Nemikolh <joan.varvenne@gmail.com>
2015-10-18 21:38:54 +0100

    Fixed a bug if this is called when there's no user.

--- server/logger/helpers.ts
+++ server/logger/helpers.ts
@@ -3,7 +3,7 @@ import {info as winfo} from 'winston';
 
 export function logRequest(message: string): RequestHandler {
     return (req, res, next) => {
-        winfo(`User: ${req.user.username} ${message}`);
+        winfo(`User: ${req.user && req.user.username} ${message}`);
         next();
     };
 }
