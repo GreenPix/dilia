a22a981fd548630787e9e9c925f68c533e6c19af
Nemikolh <joan.varvenne@gmail.com>
2015-10-25 22:24:50 +0000

    Improved the handling of files with multiples tab.

--- editor/components/rules/editor.ts
+++ editor/components/rules/editor.ts
@@ -11,6 +11,14 @@ import {RuleEditorExec} from './exec';
 let ruleEditorTemplate = require<string>('./editor.html');
 let ruleEditorCss = require<string>('./editor.css');
 
+interface FileTab {
+    index: number;
+    content: string;
+    name: string;
+    active: boolean;
+    readonly: boolean;
+}
+
 @Component({
     selector: 'rule-editor'
 })
@@ -22,10 +30,12 @@ let ruleEditorCss = require<string>('./editor.css');
 export class RuleEditor implements AfterViewInit {
 
     id: string;
-    file_list: Array<{ name: string, active: boolean }>;
+    file_list: Array<FileTab>;
+    current_file: number;
     text_area_width: number;
     text_area_height: number;
     editor: AceAjax.Editor;
+    interpreter: AaribaInterpreter;
 
     constructor(
         id: UniqueId,
@@ -34,23 +44,39 @@ export class RuleEditor implements AfterViewInit {
         private socket: SocketIOService)
     {
         this.id = id.get();
+        this.current_file = 0;
         this.file_list = [
-            { name: "test", active: true },
-            { name: "hello world", active: false },
-            { name: "foobar", active: false }
+            { index: 0, name: "test", active: true, readonly: true, content: "" },
+            { index: 1, name: "hello world", active: false, readonly: true, content: "" },
+            { index: 2, name: "foobar", active: false, readonly: false, content: "" }
         ];
         this.text_area_width = 500;
         this.text_area_height = 400;
+        this.interpreter = new AaribaInterpreter();
     }
 
-    open(event: Event, filename: string) {
+    open(event: Event, next_file: FileTab) {
         event.preventDefault();
-        console.log(filename);
+
+        // Set the previous_file content
+        let previous_file = this.file_list[this.current_file];
+        previous_file.active = false;
+        previous_file.content = this.editor.getValue();
+
+        // Open the next file
+        next_file.active = true;
+        this.current_file = next_file.index;
+        this.setFile(next_file);
+
+        console.log(next_file.name);
+    }
+
+    currentFile(): FileTab {
+        return this.file_list[this.current_file];
     }
 
     afterViewInit(): void {
         this.editor = ace.edit(this.id);
-        this.editor.setReadOnly(true);
         this.editor.setOptions({
             enableBasicAutocompletion: true,
             enableSnippets: false,
@@ -61,39 +87,49 @@ export class RuleEditor implements AfterViewInit {
         this.editor.getSession().setMode(new AaribaScriptTextMode());
         this.editor.getSession().setTabSize(2);
         this.editor.getSession().setUseSoftTabs(true);
-        // this.getLastUsedResources();
+        this.editor.addEventListener('change', (action, editor) => {
+            this.listenToChange(action, editor);
+        });
+
+        // TODO: remove this
+        this.setFile(this.file_list[this.current_file]);
+        // TODO: and use this instead:
+        //this.getLastUsedResources();
     }
 
     private getLastUsedResources(): void {
         this.http.get('/api/user/lastusedresources')
             .map(res => <any>res.json())
             .subscribe(res => {
-
+                console.log(res);
             });
     }
 
-    private listenToChange(): void {
-        let interpreter = new AaribaInterpreter();
-        this.editor.addEventListener('change', (action, editor) => {
-            try {
-                this.editor.getSession().clearAnnotations();
-                interpreter.reset();
-                interpreter.execute(editor.getValue());
-            } catch (e) {
-                let error: AaribaScriptError = e;
-                if (error.line && error.column) {
-                  this.editor.getSession().setAnnotations([
-                      {
-                          row: error.line - 1,
-                          column: error.column - 1,
-                          text: `${error.name}: ${error.message}`,
-                          type: "error",
-                      }
-                  ]);
-                } else {
-                  console.error(error);
-                }
+    private setFile(file: FileTab): void {
+        this.editor.getSession().setValue(file.content);
+        this.editor.setReadOnly(file.readonly);
+        this.listenToChange(null, this.editor);
+    }
+
+    private listenToChange(action: any, editor: any): void {
+        try {
+            this.editor.getSession().clearAnnotations();
+            this.interpreter.reset();
+            this.interpreter.execute(editor.getValue());
+        } catch (e) {
+            let error: AaribaScriptError = e;
+            if (error.line && error.column) {
+              this.editor.getSession().setAnnotations([
+                  {
+                      row: error.line - 1,
+                      column: error.column - 1,
+                      text: `${error.name}: ${error.message}`,
+                      type: "error",
+                  }
+              ]);
+            } else {
+              console.error(error);
             }
-        });
+        }
     }
 }
