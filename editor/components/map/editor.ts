3089e62ee4a56926280c844ab96c571a9461a52c
Nemikolh <joan.varvenne@gmail.com>
2015-11-29 12:12:36 +0000

    Added MapEditor to toolbar. Basic css to position the canvas.

--- editor/components/map/editor.ts
+++ editor/components/map/editor.ts
@@ -1,15 +1,51 @@
 import {Component, View} from 'angular2/angular2';
-import {CanActivate} from 'angular2/router';
+import {UniqueId} from '../../services/index';
 
 let mapEditorTemplate = require<string>('./editor.html');
+let mapEditorScss = require<Webpack.Scss>('./editor.scss');
 
 @Component({
     selector: 'map-editor',
 })
 @View({
+    styles: [mapEditorScss.toString()],
     templateUrl: mapEditorTemplate
 })
-@CanActivate(() => false)
 export class MapEditor {
 
+    private id: string;
+    private canvas: HTMLCanvasElement;
+    private gl: WebGLRenderingContext;
+    private _loop: () => void;
+
+    constructor(id: UniqueId) {
+        this.id = id.get();
+    }
+
+    afterViewInit(): void {
+        this.canvas = document.getElementById(this.id) as HTMLCanvasElement;
+        this.gl = this.canvas.getContext('webgl') as WebGLRenderingContext;
+
+        this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+
+        window.onresize = () => {
+            this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+        };
+
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.depthFunc(this.gl.LEQUAL);
+
+        this._loop = () => {
+            this.loop();
+            setTimeout(this._loop, 200);
+        };
+
+        this._loop();
+    }
+
+    loop() {
+        let gl = this.gl;
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+    }
 }
