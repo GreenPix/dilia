41fd385c9f8ad0bffc22df6593db50aba4530649
Nemikolh <joan.varvenne@gmail.com>
2016-01-11 20:04:03 +0000

    Switched to NEAREST filtering.

--- editor/gl/tex.ts
+++ editor/gl/tex.ts
@@ -23,12 +23,15 @@ export class TextureLoader {
                 gl.UNSIGNED_BYTE, img);
             if (isPowerOf2(img.width) && isPowerOf2(img.height)) {
                 gl.generateMipmap(gl.TEXTURE_2D);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST_MIPMAP_NEAREST);
             } else {
                 // Check that:
                 // No, it's not a power of 2. Turn off mips and set wrapping to clamp to edge
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
             }
             cb(texture);
         };
