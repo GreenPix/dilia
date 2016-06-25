ef042f34990edaaa9fadddcf64292a25bb506e9f
Nemikolh <joan.varvenne@gmail.com>
2016-06-25 12:10:37 +0100

    Improve by a little the build story.

--- webpack.config.js/index.js
+++ webpack.config.js/index.js
@@ -1,4 +1,62 @@
-switch (process.env.NODE_ENV) {
+'use strict';
+
+const docopt = require('docopt').docopt;
+const colors = require('colors/safe');
+const noop = require('./util').colors_noop;
+const resolve_env = require('./util').resolve_env;
+const current_env = require('./util').current_env;
+
+
+let USAGE = (colors) => `
+${colors.bold.green('Webpack build options')}
+
+Those options allows to override the default values fetch using
+the environment variable 'NODE_ENV'.
+
+Your current value is:
+
+    '${current_env()}'
+
+Thus the environment will be:
+
+    '${resolve_env()}'
+
+The following options allows you to override this behavior.
+
+${colors.bold.green('Usage:')}
+  webpack [--prod | --dev]
+  webpack --opt-help | --version
+
+${colors.bold.green('Arguments:')}
+  --opt-help            Show this message.
+  --version             Show the version of dilia.
+  --prod                Start a production build.
+  --dev                 Start a dev environment.
+  --test                Start a test environment.
+`;
+
+let options = docopt(USAGE(noop), {version: "1.0.0"});
+let mode = process.env.NODE_ENV;
+
+if (options['--opt-help']) {
+    console.log(USAGE(colors));
+    process.exit();
+}
+
+if (options['--prod']) {
+  console.log(colors.bold("Start production build"));
+  mode = 'prod';
+} else if (options['--dev']) {
+  console.log(colors.bold.yellow("Start development environment"));
+  mode = 'dev';
+} else if (options['--test']) {
+  console.log(colors.bold.green("Start test environment"));
+  mode = 'dev';
+}
+
+
+
+switch (mode) {
     case 'prod':
     case 'production':
         module.exports = require('./prod.js');
