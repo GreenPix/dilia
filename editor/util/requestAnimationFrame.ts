82d859beefe981bf2947a04004cf886bd91d8267
Nemikolh <joan.varvenne@gmail.com>
2016-09-03 15:35:07 +0100

    Fix bug with requestAnimationFrame.

--- editor/util/requestAnimationFrame.ts
+++ editor/util/requestAnimationFrame.ts
@@ -1,7 +1,7 @@
 export var requestAnimationFrame: (cb: Function) => void;
 
 if (IS_PRODUCTION) {
-    requestAnimationFrame = (window as any).unwrapedRequestAnimationFrame;
+    requestAnimationFrame = (window as any).unwrapedRequestAnimationFrame.bind(window);
 } else {
     requestAnimationFrame = (cb) => (window as any).unwrapedSetTimeout(cb, 50);
 }
