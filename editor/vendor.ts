5c9e81aae188493bd7cde17a3129e063bf303915
Nemikolh <joan.varvenne@gmail.com>
2016-09-03 11:01:04 +0100

    Fix travis. Remove unused tsconfig.

--- editor/vendor.ts
+++ editor/vendor.ts
@@ -1,8 +1,3 @@
-// For vendors for example jQuery, Lodash, angular2-jwt just import them here unless you plan on
-// chunking vendors files for async loading. You would need to import the async loaded vendors
-// at the entry point of the async loaded file. Also see custom-typings.d.ts as you also need to
-// run `typings install x` where `x` is your module
-
 // Angular 2
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
@@ -11,14 +6,7 @@ import '@angular/common';
 import '@angular/http';
 
 // RxJS
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/share';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/fromEvent';
-
+import './rxjs-add';
 import 'dragula';
 
 if (IS_PRODUCTION) {
