5349c1e583677748a641b9d2838a59c6e922416e
Nemikolh <joan.varvenne@gmail.com>
2016-09-05 10:49:06 +0100

    Fix #28 broken toolbar (RouterModule was missing).

--- editor/components/shared.module.ts
+++ editor/components/shared.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import {AlertBox} from './alert/box';
 import {AutocompleteFiles} from './autocomplete/autocomplete';
@@ -15,7 +16,7 @@ import {SelectEl, AnimFadeIn} from '../services/directives';
 import {HttpService} from '../services';
 
 @NgModule({
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, RouterModule],
     declarations: [
         AlertBox,
         AutocompleteFiles,
