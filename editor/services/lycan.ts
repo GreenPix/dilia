5b5e94f0096d7ce84e47a984eeeafad1165a0342
Vaelden <maugan.villatel@gmail.com>
2016-11-29 21:25:39 +0000

    Added serialisation of commands, and separated mock code

--- editor/services/lycan.ts
+++ editor/services/lycan.ts
@@ -9,6 +9,13 @@ export interface Point {
     y: number;
 }
 
+export const enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 export interface LycanMessagePosition {
     kind: 'position';
     entity: number;
@@ -44,6 +51,25 @@ export type LycanMessage = LycanMessagePosition | LycanMessageResponse |
     LycanMessageThisIsYou | LycanMessageNewEntity | LycanMessageEntityHasQuit;
 
 
+export interface LycanCommandAuthenticate {
+    kind: 'authenticate';
+    guid: string;
+    token: string;
+}
+
+export interface LycanOrderWalk {
+    kind: 'walk';
+    entity: number;
+    direction: Direction | null;
+}
+
+export interface LycanOrderAttack {
+    kind: 'attack';
+    entity: number;
+}
+
+export type LycanCommand = LycanCommandAuthenticate | LycanOrderWalk | LycanOrderAttack;
+
 @Injectable()
 export class LycanService {
     private observable: Observable<LycanMessage>;
@@ -62,8 +88,6 @@ export class LycanService {
         });
         this.socket.on('connect', () => {
             console.log('Connected to Lycan Proxy');
-            let authenticate = '{"GameCommand":{"Authenticate":["00000032-0000-0000-0000-000000000000","50"]}}';
-            this.socket.send(authenticate);
         });
         this.socket.on('message', (message) => {
             if (!this.subscriber) return;
@@ -74,16 +98,47 @@ export class LycanService {
             }
         });
 
-        // TODO: Remove this mock subscription
+        this.mockThings();
+    }
+
+    getMessages(): Observable<LycanMessage> {
+        return this.observable;
+    }
+
+    sendCommand(command: LycanCommand) {
+        let message = serialize(command);
+        this.socket.send(message);
+    }
+
+    // Mock code to be removed later
+    private mockThings() {
         this.observable.subscribe({
             next: (message) => {
                 // Position message are just too frequent
                 if (message.kind != 'position') {
                     console.log(`Parsed message: ${JSON.stringify(message)}`);
                 }
+
+                // Start walking when receiving ThisIsYou
+                if (message.kind == 'thisIsYou') {
+                    let me = message.entity;
+                    let goUp: LycanOrderWalk = {
+                        kind: 'walk',
+                        entity: me,
+                        direction: Direction.Up,
+                    };
+                    this.sendCommand(goUp);
+                }
             },
             error: (err) => { console.log('An error occured ' + err); },
         });
+
+        let authenticate: LycanCommandAuthenticate = {
+            kind: 'authenticate',
+            guid: '00000032-0000-0000-0000-000000000000',
+            token: '50',
+        };
+        this.sendCommand(authenticate);
     }
 }
 
@@ -125,4 +180,57 @@ function parse(message: string): LycanMessage | null {
 
     console.log(`Warning: could not parse ${message}`);
     return null;
+}
+
+function serialize(command: LycanCommand): string {
+    let json;
+    switch (command.kind) {
+        case 'authenticate': {
+            json = {
+                GameCommand: {
+                    Authenticate: [command.guid, command.token],
+                }
+            };
+            break;
+        }
+
+        case 'walk': {
+            // WHY did I take North/South/East/West? It is stupid ...
+            let direction;
+            switch (command.direction) {
+                case Direction.Up:
+                    direction = 'North';
+                    break;
+                case Direction.Down:
+                    direction = 'South';
+                    break;
+                case Direction.Left:
+                    direction = 'West';
+                    break;
+                case Direction.Right:
+                    direction = 'East';
+                    break;
+            }
+            json = {
+                EntityOrder: {
+                    entity: command.entity,
+                    order: {
+                        Walk: direction,
+                    }
+                }
+            };
+            break;
+        }
+
+        case 'attack': {
+            json = {
+                EntityOrder: {
+                    entity: command.entity,
+                    order: 'Attack',
+                }
+            };
+            break;
+        }
+    }
+    return JSON.stringify(json);
 }
