723d4b7bb252c8b5ba774388f2c2da801df21e32
Vaelden <maugan.villatel@gmail.com>
2016-11-29 21:59:16 +0000

    Start injecting LycanService at what seems the right place

--- editor/modules/runner.module.ts
+++ editor/modules/runner.module.ts
@@ -1,13 +1,19 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../components/shared.module';
+import {LycanService} from '../services/lycan';
 
 import {GameCanvas} from './runner/game-canvas.component';
+import {GameState} from './runner/game-state';
 
 
 @NgModule({
     imports: [SharedModule],
     declarations: [
         GameCanvas
+    ],
+    providers: [
+        LycanService,
+        GameState,
     ],
 })
 export class RunnerModule {}
